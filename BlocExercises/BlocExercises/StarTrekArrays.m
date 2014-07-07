b3e0cae8f451a1e585778a5164aa919a9905204b
Jeanne Sneed <jsneed@gmail.com>
2014-07-07 14:47:04 -0700

    Completed arrays

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -11,22 +11,27 @@
 @implementation StarTrekArrays
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
-    /* WORK HERE */
-    return @[];
+    NSArray *arr = [characterString componentsSeparatedByString:@";"];
+    return arr;
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @"";
+    NSString *str = [characterArray componentsJoinedByString:@";"];
+    return str;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @[];
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+    NSMutableArray *cArray = [(NSArray*)characterArray mutableCopy];
+    [cArray sortUsingDescriptors:@[sortDescriptor]];
+    return cArray;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
-    /* WORK HERE */
+    NSMutableArray *cArray = [(NSArray*)characterArray mutableCopy];
+    NSPredicate *containsWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'worf'"];
+    [cArray filterUsingPredicate:containsWorf];
+    if(cArray.count > 0) return YES;
     return NO;
 }
 

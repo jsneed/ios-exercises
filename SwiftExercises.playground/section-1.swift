05fd5a7d567ec10b6f6f645a4943ac862c646aaf
Jeanne Sneed <jsneed@gmail.com>
2014-07-19 17:25:23 -0700

    Complete Swift Exercises

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -8,7 +8,7 @@ Strings
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
     // WORK HERE
-    return cheese
+    return "My favorite cheese is " + cheese + "."
 }
 
 let fullSentence = favoriteCheeseStringWithCheese("cheddar")
@@ -20,13 +20,17 @@ Arrays & Dictionaries
 
 */
 
-let numberArray = [1, 2, 3, 4]
+var numberArray = [1, 2, 3, 4]
 // Add 5 to this array
 // WORK HERE
+numberArray.append(5)
+numberArray
 
-let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+var numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
 // Add 5 : "five" to this dictionary
 // WORK HERE
+numberDictionary[5] = "five"
+numberDictionary
 
 /*
 
@@ -36,9 +40,17 @@ Loops
 
 // Use a closed range loop to print 1 - 10, inclusively
 // WORK HERE
+for index in 1...10 {
+    println("hi \(index)")
+    index
+}
 
 // Use a half-closed range loop to print 1 - 10, inclusively
 // WORK HERE
+for index in 1..<10 {
+    println("hi2 \(index)")
+    index
+}
 
 let worf = [
     "name": "Worf",
@@ -58,7 +70,15 @@ let characters = [worf, picard]
 func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
     // WORK HERE
-    return []
+    var drinkArray : [String] = []
+    for chars in characters {
+        for (key, value) in chars {
+            if key == "favorite drink" {
+                drinkArray.append(value)
+            }
+        }
+    }
+    return drinkArray
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
@@ -76,6 +96,18 @@ Functions
 let strings = ["milk", "eggs", "bread", "challah"]
 
 // WORK HERE - make your function and pass `strings` in
+func concatStringArray(strArray:Array<String>) -> String {
+    var myString : String = ""
+    for var i = 0; i < strArray.count; i++ {
+        if i != 0 {
+            myString = myString + ";"
+        }
+        myString = myString + strArray[i]
+    }
+    return myString
+}
+
+let actualOutput = concatStringArray(strings)
 
 let expectedOutput = "milk;eggs;bread;challah"
 
@@ -85,7 +117,10 @@ Closures
 
 */
 
-let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All Berries", "Cookie Crisp"]
+var cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All Berries", "Cookie Crisp"]
 
 // Use a closure to sort this array alphabetically
 // WORK HERE
+var cerealArraySorted = sorted(cerealArray, {(cereal1: String, cereal2: String) -> Bool in
+    return cereal1 < cereal2
+})
